1a9875027654a3082433a1a3f3fdf9c3799c6cb1
Valmir Paiva Stachin <valmir@unimake.com.br>
2025-08-28 13:50:38 -0300

    feat: Adiciona projeto da API Etapa 4

---
+++ etapa_4/src/modules/contatos/contato.controller.js
@@ -0,0 +1,48 @@
+export class ContatoController {
+  constructor(contatoService) {
+    this.contatoService = contatoService;
+  }
+
+  async getContatos(request, reply) {
+    const contatos = this.contatoService.getAllContatos();
+    return reply.send(contatos);
+  }
+
+  async getContatoById(request, reply) {
+    const { id } = request.params;
+    const contato = this.contatoService.getContatoById(id);
+
+    if (!contato) {
+      return reply.code(404).send({ message: "Contato não encontrado" });
+    }
+    return reply.send(contato);
+  }
+
+  async createContato(request, reply) {
+    const novoContato = this.contatoService.createContato(request.body);
+    return reply.code(201).send(novoContato);
+  }
+
+  async updateContato(request, reply) {
+    const { id } = request.params;
+    const contatoAtualizado = this.contatoService.updateContato(
+      id,
+      request.body
+    );
+
+    if (!contatoAtualizado) {
+      return reply.code(404).send({ message: "Contato não encontrado" });
+    }
+    return reply.send(contatoAtualizado);
+  }
+
+  async deleteContato(request, reply) {
+    const { id } = request.params;
+    const sucesso = this.contatoService.deleteContato(id);
+
+    if (!sucesso) {
+      return reply.code(404).send({ message: "Contato não encontrado" });
+    }
+    return reply.code(204).send();
+  }
+}
